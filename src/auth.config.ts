7701f05aaaf366548b614e3c2d91bea89429cf4c
Mohamed780050 <mohamed39omer@gmail.com>
2025-06-15 10:01:17 -0400

    know signin or up using OAuth serivice is ok

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,9 +3,19 @@ import type { NextAuthConfig, User } from "next-auth";
 import { loginSchema } from "./Schema/validation";
 import bcrypt from "bcryptjs";
 import user from "../data/user";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials): Promise<User | null> {
         try {
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,8 +2,12 @@
 import { Button } from "./ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { signIn } from "next-auth/react";
 
 function Social() {
+  function onClickHanler(proiver: "google" | "github") {
+    signIn(proiver);
+  }
   return (
     <div className="flex items-center w-full gap-x-2">
       {/* 
@@ -13,7 +17,7 @@ function Social() {
         size="icon"
         className="w-full text-xl"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClickHanler("google")}
       >
         <FcGoogle className="h-8 w-8" />
       </Button>
@@ -21,7 +25,7 @@ function Social() {
         size="icon"
         className="w-full text-xl"
         variant="outline"
-        onClick={() => {}}
+        onClick={() => onClickHanler("github")}
       >
         <FaGithub className="h-8 w-8" />
       </Button>
