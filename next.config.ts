7038f73d441aba11edd6596659ad37232919dea8
Mohamed780050 <mohamed39omer@gmail.com>
2025-07-26 14:45:31 +0300

    fixed linting problem

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    // Allow production builds even if ESLint errors are present
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
